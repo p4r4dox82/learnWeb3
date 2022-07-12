2427a789d99ff98bc550c9fc47472578c5535a74
p4r4dox82 <p4r4dox82@snu.ac.kr>
2022-07-12 18:52:42 +0900

    ICO

---
+++ sophomore/ICO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xCD50BC7dee306B9789a9C4130b36512db821051A";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };  
